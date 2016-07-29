6fbebbc749662e7c5dd47c9e7fc1c6ea60546e68
Ryan Murphy <murjax@gmail.com>
2016-07-29 16:55:25 +0000

    Basic ActionCable authentication (untested, may be changed later)

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,16 @@
 module ApplicationCable
 	class Connection < ActionCable::Connection::Base
+		identified_by :current_user
+		def connect
+			if current_user
+				current_user
+			else
+				reject_unauthorized_connection
+			end
+		end
 
+		def disconnect
+
+		end
 	end
 end
