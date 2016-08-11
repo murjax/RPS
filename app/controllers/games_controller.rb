ec2e4b1d0b78f32940633a22747538ccc40de5bd
Ryan Murphy <murjax@gmail.com>
2016-08-11 20:26:08 +0000

    Create basic game form on Games#edit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,15 +5,48 @@ class GamesController < ApplicationController
 	def new
 		@games = Game.all
 		@game = Game.new
+		@game.choices.build
+	end
+
+	def edit
+		@game = Game.find(params[:id])
 	end
 
 	def create
 		logger.debug("create")
-		@game = Game.create!(player_one_id: params[:game][:player_one_id], 
-			player_two_id: params[:game][:player_two_id], winner_id: params[:game][:winner_id])
-		ActionCable.server.broadcast "games", render( partial: 'games/game', object: @game )
+
+		@game = Game.create!
+		logger.debug(@game)
+		redirect_to(:action => 'edit', id: @game.id)
+	end
+
+	def update
+		@game = Game.find(params[:id])
+		
+		
+		
+		if @game.choices.count == 1
+			if @game.choices[0].user_id != current_user.id
+				@game.choices.create!(game_params[:choices_attributes]["0"])
+				ActionCable.server.broadcast "games", render( partial: 'games/game', object: @game )
+
+			end
+		elsif @game.choices.count == 0
+			@game.choices.create!(game_params[:choices_attributes]["0"])
+			ActionCable.server.broadcast "games", render( partial: 'games/game', object: @game )
+		end
+
+		
+
+		
 	end
 
 	def show
+	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(choices_attributes: [:id, :user_id, :choice])
 	end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
 	has_many :choices
+	accepts_nested_attributes_for :choices, allow_destroy: true
 end
