3d4698a76ffdf58317e41f09ee0017e0552adf95
Ryan Murphy <murjax@gmail.com>
2016-08-12 17:17:20 +0000

    Create Game model spec

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,30 @@
+describe "Game" do 
+	user = create(:user)
+	let(:game) {Game.new}
+	let(:rock) {game_id: game.id, user_id: 1, choice: "Rock"}
+	let(:paper) {game_id: game.id, user_id: 2, choice: "Paper"}
+	let(:scissors) {game_id: game.id, user_id: 3, choice: "Scissors"}
+	scenario "Game has no more than two choices" do
+		game.choices.create!(rock)
+		game.choices.create!(paper)
+		game.choices.create!(scissors)
+
+		expect(game.choices.count).to eq(2)
+	end
+
+	scenario "Game players have numerical IDs" do 
+		game.player_one_id = user.id
+		expect(game.player_one_id).should be_kind_of(Fixnum)
+	end
+
+	scenario "Game winner has numerical ID" do
+		game.winner_id = user.id
+		expect(game.winner_id).should be_kind_of(Fixnum)
+	end
+
+	scenario "When a second choice is submitted, a winner should be determined" do
+		game.choices.create!(rock)
+		game.choices.create!(paper)
+		expect(game.winner_id).to eq(2)
+	end
+end
