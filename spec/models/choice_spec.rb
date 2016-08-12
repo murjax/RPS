91c20f2a34ebb9c0c0c96789193ffef0c3d4d7ab
Ryan Murphy <murjax@gmail.com>
2016-08-12 17:36:14 +0000

    Create Choice model spec

--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
+let(:choice) {Choice.new}
+describe "Choice" do 
+	describe "Choice should be either 'Rock', 'Paper', or 'Scissors'" do
+		scenario "Choice is Rock"
+			choice.choice = "Rock"
+			choice.user_id = 1
+			expect(choice.save).to eq(true)
+		end
 
-RSpec.describe Choice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+		scenario "Choice is Paper"
+			choice.choice = "Paper"
+			choice.user_id = 1
+			expect(choice.save).to eq(true)
+		end
+
+		scenario "Choice is Scissors"
+			choice.choice = "Scissors"
+			choice.user_id = 1
+			expect(choice.save).to eq(true)
+		end
+
+		scenario "Choice is Tree"
+			choice.choice = "Tree"
+			choice.user_id = 1
+			expect(choice.save).to eq(false)
+		end
+	end
+
+	describe "Choice should have a User id" do
+		scenario "Choice is saved with User id" do
+			choice.choice = "Rock"
+			choice.user_id = 1
+			expect(choice.save).to eq(true)
+		end
+
+		scenario "Choice is saved without User id" do
+			choice.choice = "Rock"
+			expect(choice.save).to eq(false)
+		end
+
+	end
 end
+
+
