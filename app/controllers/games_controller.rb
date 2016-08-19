7392476dde7d97c603c8c265aec8dcda9c1abc8e
Ryan Murphy <murjax@gmail.com>
2016-08-19 00:35:54 +0000

    Create set_winner method and use when second choice is posted

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -28,8 +28,8 @@ class GamesController < ApplicationController
 		if @game.choices.count == 1
 			if @game.choices[0].user_id != current_user.id
 				@game.choices.create!(game_params[:choices_attributes]["0"])
+				@game.set_winner
 				ActionCable.server.broadcast "games", render( partial: 'games/game', object: @game )
-
 			end
 		elsif @game.choices.count == 0
 			@game.choices.create!(game_params[:choices_attributes]["0"])
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,24 @@
 class Game < ActiveRecord::Base
 	has_many :choices
 	accepts_nested_attributes_for :choices, allow_destroy: true
+
+
+	def set_winner
+		choices = self.choices.map{ |c| c.choice }
+
+		if choices[0] == "Rock" && choices[1] == "Scissors"
+			self.winner_id = self.choices[0].user_id
+		elsif choices[0] == "Scissors" && choices[1] == "Rock"
+			self.winner_id = self.choices[1].user_id
+		elsif choices[0] == "Paper" && choices[1] == "Rock"
+			self.winner_id = self.choices[0].user_id
+		elsif choices[0] == "Rock" && choices[1] == "Paper"
+			self.winner_id = self.choices[1].user_id
+		elsif choices[0] == "Scissors" && choices[1] == "Paper"
+			self.winner_id = self.choices[0].user_id
+		elsif choices[0] == "Paper" && choices[1] == "Scissors"
+			self.winner_id = self.choices[1].user_id
+		end
+
+	end
 end
