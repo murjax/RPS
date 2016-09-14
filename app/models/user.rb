4c6cb12492eee01b645d5fc2b03fb5e2ba622ee1
Ryan Murphy <murjax@gmail.com>
2016-09-14 16:54:18 +0000

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9]+\Z/, message: "must
+  	only contain alphanumeric characters" }
+  validates :email, presence: true, uniqueness: true, format: { with: Devise::email_regexp, message: "must be
+  	in format of 'user@example.com'" }
+
   def increase_rating
   	self.rating += 1
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,44 @@ require 'rails_helper'
 
 describe "User" do 
 	
-	scenario "User should have alphanumeric username" do
-		user = create(:user)
+	scenario "Expected user credentials should be valid" do
+		user = User.new(username: "someuser", email: "email@example.com", password: "password123")
 		expect(user).to be_valid
 	end
 
-	scenario "User should have email in format of 'email@example.com'" do
-		user = create(:user)
-		expect(user).to be_valid
+	scenario "User must have username" do
+		user = User.new(email: "email@example.com", password: "password123")
+		expect(user).to_not be_valid
 	end
 
-	scenario "User should have password that is at least 6 characters long" do
-		user = create(:user)
-		expect(user).to be_valid
+	scenario "Non-alphanumeric characters are not permitted in a username" do
+		user = User.new(username: "some_user", email: "email@example.com", password: "password123")
+		expect(user).to_not be_valid
+	end
+
+	scenario "User must have an email" do
+		user = User.new(username: "someuser", password: "password123")
+		expect(user).to_not be_valid
+	end
+
+	scenario "Non-email formats are not permitted as email" do
+		user = User.new(username: "someuser", email: "email_at_example.com", password: "password123")
+		expect(user).to_not be_valid
+	end
+
+	scenario "User must have a password" do
+		user = User.new(username: "someuser", email: "email@example.com")
+		expect(user).to_not be_valid
+	end
+
+	scenario "Password must be at least 6 characters long" do
+		user = User.new(username: "someuser", email: "email_at_example.com", password: "123")
+		expect(user).to_not be_valid
+	end
+
+	scenario "Must have a rating" do
+		user = User.new(username: "someuser", email: "email_at_example.com", password: "123")
+		expect(user.rating).to_not be_nil
 	end
 
 end
